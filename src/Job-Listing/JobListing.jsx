b4db86489220463547cc79dc173b10be097482e5
Nitish Kumar Yadav <engineer.nitish7115@gmail.com>
2023-03-13 18:23:13 +0530

    Fixed Color and Search Bar

--- src/Job-Listing/JobListing.jsx
+++ src/Job-Listing/JobListing.jsx
@@ -5,7 +5,8 @@ const JobListing = (props) => {
   const dispatch = useDispatch();
   let borderLeft = "";
   const data = props.data;
-
+  const tech =
+    "p-1 bg-bodyBg border-transparent hover:bg-teal-500 hover:text-white";
   if (data.featured) {
     borderLeft = "border-l-cyan-500";
   } else {
@@ -18,10 +19,10 @@ const JobListing = (props) => {
   }
   return (
     <div
-      className={`flex text-cyan-500 bg-white font-medium border-transparent border-2 ${borderLeft} rounded-md mt-10 xs:flex-col pl-4 relative md:flex-row md:justify-around md:min-w-4/5 md:flex-grow-1.5 md:pl-0 md:gap-5`}
+      className={`flex text-teal-500 bg-white font-medium border-transparent border-2 ${borderLeft} rounded-md mt-14 xs:flex-col pl-4 xs:relative md:flex-row md:justify-around md:min-w-4/5 md:flex-grow-1.5 md:pl-0 md:gap-5`}
     >
       <div className="flex xs:flex-col mt-10 md:flex-row md:m-5">
-        <div className="xs:w-2/12 xs:absolute xs:-top-7 md:static md:mx-auto md:w-20 md:self-center">
+        <div className="xs:w-2/12 xs:absolute xs:-top-8 md:static md:mx-auto md:w-20 md:self-center">
           <img src={data.logo} alt={data.company} />
         </div>
 
@@ -30,17 +31,19 @@ const JobListing = (props) => {
             <p className="">{data.company}</p>
             <ul className="flex gap-2.5 text-white">
               {data.new && (
-                <li className="p-0.5 px-1.5 rounded-2xl bg-cyan-500">New!</li>
+                <li className="p-0.5 px-1.5 rounded-2xl bg-teal-500">New!</li>
               )}
               {data.featured && (
-                <li className="p-0.5 px-1.5 rounded-2xl bg-cyan-500 bg-black">
+                <li className="p-0.5 px-1.5 rounded-2xl bg-teal-500 bg-black">
                   Featured
                 </li>
               )}
             </ul>
           </div>
           <div className="flex mt-2">
-            <h2 className="text-black font-bold">{data.position}</h2>
+            <h2 className="text-black font-bold hover:text-teal-500">
+              {data.position}
+            </h2>
           </div>
 
           <div className="flex mt-2">
@@ -57,35 +60,21 @@ const JobListing = (props) => {
         <hr className="h-px bg-gray-400 border-0 dark:bg-gray-400" />
       </div>
 
-      <div className="flex text-cyan-500 xs:pr-3 xs:mt-4 xs:mb-6 md:content-center md:flex-wrap md:min-w-min md:justify-end">
+      <div className="flex text-teal-500 xs:pr-3 xs:mt-4 xs:mb-6 md:content-center md:flex-wrap md:min-w-min md:justify-end">
         <ul className="flex flex-wrap gap-2.5">
-          <li
-            className="p-1 bg-bodyBg border-transparent"
-            onClick={onClickHandler}
-          >
+          <li className={`${tech}`} onClick={onClickHandler}>
             {data.role}
           </li>
-          <li
-            className="p-1 bg-bodyBg border-transparent"
-            onClick={onClickHandler}
-          >
+          <li className={`${tech}`} onClick={onClickHandler}>
             {data.level}
           </li>
           {data.tools.map((tool, index) => (
-            <li
-              key={index}
-              className="p-1 bg-bodyBg border-transparent"
-              onClick={onClickHandler}
-            >
+            <li key={index} className={`${tech}`} onClick={onClickHandler}>
               {tool}
             </li>
           ))}
           {data.languages.map((language, index) => (
-            <li
-              key={index}
-              className="p-1 bg-bodyBg border-transparent"
-              onClick={onClickHandler}
-            >
+            <li key={index} className={`${tech}`} onClick={onClickHandler}>
               {language}
             </li>
           ))}
--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -21,19 +21,19 @@ const SearchBar = (props) => {
   if (isSearchEnabled) {
     return (
       <div className="flex bg-white rounded-md font-leagueSparten justify-between gap-5 border-transparent xs:w-12/12 p-4 md:w-[inherit]">
-        <div className="flex flex-wrap gap-5 justify-start m-1.5 max-w-md w-12/12">
+        <div className="flex flex-wrap gap-5 justify-start m-1.5 w-12/12">
           {keywords.map((data, index) => (
-            <div key={index} className="flex border-red-50 rounded-2xl">
-              <p className="bg-bodyBg font-leagueSparten text-cyan-500 xs:font-medium p-1.5 text-base">
+            <div key={index} className="flex rounded-2xl">
+              <p className="bg-bodyBg font-leagueSparten text-teal-500 xs:font-medium p-1.5 text-base md:font-bold">
                 {data}
               </p>
               <button
                 onClick={onClickHandler}
                 data-keyword={data}
-                className="flex content-center justify-center w-8 flex-wrap bg-cyan-500 xs:w-5"
+                className="flex content-center justify-center w-8 flex-wrap bg-teal-500 xs:w-5 md:w-8 hover:bg-black"
               >
                 <img
-                  className="xs:w-3"
+                  className="xs:w-3 md:w-[initial]"
                   src="images/icon-remove.svg"
                   alt="remove"
                   data-keyword={data}
@@ -42,7 +42,12 @@ const SearchBar = (props) => {
             </div>
           ))}
         </div>
-        <button onClick={clearHandler}>Clear</button>
+        <button
+          className="text-teal-500 hover:underline md:font-bold"
+          onClick={clearHandler}
+        >
+          Clear
+        </button>
       </div>
     );
   }
